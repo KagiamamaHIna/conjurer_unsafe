7296d7fc6742eff9d5cbaf4e818178d819d5552d
KagiamamaHIna <kagiyamahino@gmail.com>
2024-12-24 12:52:37 +0800

    v2

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ if not ModIsEnabled("conjurer_reborn") then
 	return
 end
 print("Conjurer reborn unsafe loaded!")
-ConjurerRebornUnsafeVer = 1
+ConjurerRebornUnsafeVer = 2
 
 dofile_once("mods/conjurer_reborn/unsafe_init.lua")
